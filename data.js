d3b7ab69e153cc4e3263b11966360c0967794fe3
UrHardcore <90009534+UrHardcore@users.noreply.github.com>
2026-05-04 12:12:28 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,7 @@
+const vaccines = [
+  { name: "BCG", age: 0, done: false },
+  { name: "Hepatitis B", age: 0, done: false },
+  { name: "Pentavalente", age: 2, done: false },
+  { name: "Rotavirus", age: 2, done: false },
+  { name: "Triple Viral", age: 12, done: false }
+];
