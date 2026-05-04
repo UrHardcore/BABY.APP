ce44fffb2aca1e7b70c6cba21d4b586a57098f68
UrHardcore <90009534+UrHardcore@users.noreply.github.com>
2026-05-04 12:18:24 -0300

    Delete data.js

--- data.js
+++
@@ -1,7 +0,0 @@
-const vaccines = [
-  { name: "BCG", age: 0, done: false },
-  { name: "Hepatitis B", age: 0, done: false },
-  { name: "Pentavalente", age: 2, done: false },
-  { name: "Rotavirus", age: 2, done: false },
-  { name: "Triple Viral", age: 12, done: false }
-];
